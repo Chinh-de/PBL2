2d33ebcf3e66dd5b7050b524e2fde55cafc59dc9
Chinh-de <chinhde29@gmail.com>
2023-11-08 10:37:07 +0700

    ngay0711

--- Person.cpp
+++ Person.cpp
@@ -1,12 +1,74 @@
 #include "Person.h"
-Person::Person(int ID, string name, string phoneNumber, string email, bool gender, string address)
-    :ID(ID), Name(name), PhoneNumber(phoneNumber), Email(email), Gender(gender), Address(address)
+Person::Person(int ID, string name, string phoneNumber, string email, Gender gender, string address)
+    :ID(ID), name(name), phone(phoneNumber), email(email), gender(gender), address(address)
 { }
+
 Person::void Show()
 {
-    cout << "; Ten: " << this->Name << "; So Dien Thoai: " << this->PhoneNumber;
+    cout << "; Ten: " << this->name << "; So Dien Thoai: " << this->phone;
     cout << "; Gioi tinh: ";
-    if(this->Gender) cout << "Nam ;";
-    else cout << "Nu ;";
-    cout << " email: " << email << "; Dia chi: " << Address; 
+    if(this->gender == male) cout << "Nam ;";
+    if(this->gender == female)cout << "Nu ;";
+    if(this->gender == orther)cout << "Khac ;";
+    cout << " email: " << email << "; Dia chi: " << address; 
+}
+
+Person:: int getID()
+{
+    return this->ID;
+}
+
+Person:: void setID(int newID)
+{
+    this->ID = newID;
+}
+
+Person:: string getname()
+{
+    return this->name;
+}
+
+Person:: void setname(string newName)
+{
+    this->name = newname;
+}
+
+Person:: string getphone()
+{    
+    return this->phone;
+}
+
+Person:: void setphone(string newPhone)
+{
+    this->phone = newPhone;
+}
+
+Person:: string getemail()
+{
+    return this->email;
+}
+
+Person:: void setemail(string newEmail)
+{
+    this->email = newEmail;
+}
+
+Person:: Gender getgender()
+{
+    return this->gender;
+}
+
+Person:: void setgender(Gender newGender)
+{
+    this->gender = newGender;
+}
+
+Person:: string getaddress()
+{
+    return this->address;
+}
+
+Person:: void setaddress(string newAddress)
+{
+    this->address = newAddress;
 }
--- Person.h
+++ Person.h
@@ -1,14 +1,35 @@
 #include <iostream>
 using namespace std;
+#include <string>
+
+enum Gender 
+{
+    male,
+    female,
+    other
+};
 
 class Person{
     protected:
         int ID;
-        string Name, Phone, Email;
-        bool Genre;
-        string Address;
+        string name, phone, email;
+        bool gender;
+        string address;
     public:
-        Person(int = 0, string = "", string "", string = "", bool = true, string = "");
+        Person(int = 0, string = "", string "", string = "", Gender = male, string = "");
         ~Person();
         void Show();
+        int getID();
+        void setID(int);
+        string getname();
+        void setname(string);
+        string getphone();
+        void setphone(string);
+        string getemail();
+        void setemail(string);
+        Gender getgender();
+        void setgender(Gender);
+        string getaddress();
+        void setaddress(string);
+        
 };
